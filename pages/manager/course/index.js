e60af7a6f361333a11bf756916ca367572e5f0b2
MinhNguyenLe <minh.nguyenle1809@gmail.com>
2021-10-27 15:16:23 +0700

    add i18n for nextjs

---
+++ pages/manager/course/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+// components
+
+import AddClass from "components/Forms/AddCourse.js";
+import ClassList from "components/Course/CourseList.js";
+
+// layout for page
+
+import Admin from "layouts/Admin.js";
+
+export default function Classes() {
+  return (
+    <>
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-8/12 px-4">
+          <AddClass />
+        </div>
+        <div className="w-full lg:w-4/12 px-4">
+          <ClassList />
+        </div>
+      </div>
+    </>
+  );
+}
+
+Classes.layout = Admin;
